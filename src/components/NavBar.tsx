9cd432952bd9c7ae8ed5b8a8ae8ef6e8d85ec979
sameerasw <sameera.whoami@gmail.com>
2025-02-03 02:10:56 +0530

    App theme and navbar updates

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, TextField, Button, IconButton, Badge, useTheme } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { UserState } from '../store/AuthState';
@@ -9,9 +10,11 @@ import { useGetCartItemsByCustomerIdQuery } from '../api/cartApiSlice';
 interface NavBarProps {
   searchTerm?: string;
   onSearchChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  backEnabled?: boolean;
+  accountVisible?: boolean;
 }
 
-const NavBar: React.FC<NavBarProps> = ({ searchTerm, onSearchChange }) => {
+const NavBar: React.FC<NavBarProps> = ({ searchTerm, onSearchChange, backEnabled = false, accountVisible = true }) => {
   const theme = useTheme();
   const navigate = useNavigate();
   const { userId: customerId, name } = useSelector((state: { auth: UserState }) => state.auth);
@@ -26,8 +29,13 @@ const NavBar: React.FC<NavBarProps> = ({ searchTerm, onSearchChange }) => {
   };
 
   return (
-    <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
+    <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1, boxShadow: 'none'}}>
       <Toolbar>
+        {backEnabled && (
+          <IconButton onClick={() => navigate('/')}>
+            <ArrowBackIcon sx={{ color: 'white' }} />
+          </IconButton>
+        )}
         <Typography variant="h5" sx={{ ml: 2, flexGrow: 1 }}>
           My Cart App
         </Typography>
@@ -39,28 +47,38 @@ const NavBar: React.FC<NavBarProps> = ({ searchTerm, onSearchChange }) => {
             size='small'
             value={searchTerm}
             onChange={onSearchChange}
-            sx={{ width: '300px', marginLeft: 'auto', marginRight: '10px' }}
+            sx={{ 
+              width: '300px', 
+              marginLeft: 'auto', 
+              marginRight: '10px',
+              backgroundColor: 'white',
+              borderRadius: '10px'
+            }}
           />
         )}
-        {name ? (
-          <Button variant="contained" onClick={() => navigate('/login')}>
-            {name}
-          </Button>
-        ) : (
+        {accountVisible && (
           <>
-            <Button variant="contained" onClick={() => navigate('/login')} sx={{ marginRight: 1 }}>
-              Login
-            </Button>
-            <Button variant="contained" onClick={() => navigate('/register')}>
-              Register
-            </Button>
+            {name ? (
+              <Button variant="contained" onClick={() => navigate('/login')}>
+                {name}
+              </Button>
+            ) : (
+              <>
+                <Button variant="contained" onClick={() => navigate('/login')} sx={{ marginRight: 1 }}>
+                  Login
+                </Button>
+                <Button variant="contained" onClick={() => navigate('/register')}>
+                  Register
+                </Button>
+              </>
+            )}
+            <IconButton size="large" color="inherit" onClick={toCart}>
+              <Badge badgeContent={cartItems.reduce((total, item) => total + item.quantity, 0)} color="secondary">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
           </>
         )}
-        <IconButton size="large" color="inherit" onClick={toCart}>
-          <Badge badgeContent={cartItems.reduce((total, item) => total + item.quantity, 0)} color="secondary">
-            <ShoppingCartIcon />
-          </Badge>
-        </IconButton>
       </Toolbar>
     </AppBar>
   );
